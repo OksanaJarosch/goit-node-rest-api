fc63039fe496603ec46a723f3575016c157372bd
Oksana Jarosch <ok.jarosch@gmail.com>
2024-02-03 16:54:54 +0100

    pagination

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,8 +4,10 @@ const { ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
     const { _id: owner } = req.user;
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
 
-    const result = await Contact.find({owner});
+    const result = await Contact.find({owner}, "-createdAt, -updatedAt", {skip, limit});
     res.json(result);
 };
 
