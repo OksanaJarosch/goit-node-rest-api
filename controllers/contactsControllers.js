9c1869b6106e40070258c1900da19241f0883d2a
Oksana Jarosch <ok.jarosch@gmail.com>
2024-02-03 23:12:47 +0100

    filter/favorite contacts

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,10 +4,15 @@ const { ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 10 } = req.query;
+    const { page = 1, limit = 10, favorite } = req.query;
     const skip = (page - 1) * limit;
+    const favoriteFilter = { owner };
 
-    const result = await Contact.find({owner}, "-createdAt, -updatedAt", {skip, limit});
+    if (favorite) {
+        favoriteFilter.favorite = favorite
+    }
+
+    const result = await Contact.find(favoriteFilter, "-createdAt -updatedAt", {skip, limit}).populate("owner", "email");
     res.json(result);
 };
 
